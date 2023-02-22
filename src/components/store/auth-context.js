476056fc783867e93b4b68cd899e9116ef010347
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-22 11:56:53 +0300

    Created the initialToken const and pointed it to the token key

--- src/components/store/auth-context.js
+++ src/components/store/auth-context.js
@@ -9,6 +9,8 @@ const AuthContext = createContext({
 
 export const AuthContextProvider = (props) =>
 {
+    const initialToken = localStorage.getItem('token')
+
     const [token, setToken] = useState(null)
 
     const userIsLoggedIn = !!token
