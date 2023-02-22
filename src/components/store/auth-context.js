d90f97c79bdba81ecf74498920af92a3892ef515
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-22 12:29:58 +0300

    Called the setTimeout function with the logoutHandler as the function and the remainingTime as the time

--- src/components/store/auth-context.js
+++ src/components/store/auth-context.js
@@ -38,7 +38,7 @@ export const AuthContextProvider = (props) =>
 
         const remainingTime = calculateRemainingTime(expirationTime)
 
-
+        setTimeout(logoutHandler, remainingTime)
     }
 
     const contextValue = {
