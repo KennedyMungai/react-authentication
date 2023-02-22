b9c5e8722ec3708cd4f0a3358534733740fcddbf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-22 12:52:59 +0300

    Created the retrieveSToredToken function

--- src/components/store/auth-context.js
+++ src/components/store/auth-context.js
@@ -19,6 +19,12 @@ const calculateRemainingTime = (expirationTime) =>
     return remainingTime
 }
 
+const retrieveStoredToken = () =>
+{
+    const storedToken = localStorage.getItem('token')
+    const storedExpirationDate =  
+}
+
 export const AuthContextProvider = (props) =>
 {
     const initialToken = localStorage.getItem('token')
