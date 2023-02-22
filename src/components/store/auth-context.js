bde1c7f22a0f246a3d269d3e022a3df2adc7c92a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-22 09:18:44 +0300

    Created the userIsLogginIn const which extracts a boolean on the token state

--- src/components/store/auth-context.js
+++ src/components/store/auth-context.js
@@ -11,6 +11,8 @@ const AuthContextProvider = (props) =>
 {
     const [token, setToken] = useState(null)
 
+    const userIsLoggedIn = !!token
+
     return <AuthContext.Provider>
         {props.children}
     </AuthContext.Provider>
