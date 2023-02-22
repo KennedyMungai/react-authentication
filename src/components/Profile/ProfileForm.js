99399900ee2ca03e4a7b5fee1f617761c11f2c67
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-22 10:19:44 +0300

    Wired up the newPasswordINputRef to the password input tag

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -9,7 +9,11 @@ const ProfileForm = () =>
     <form className={classes.form}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input
+          type='password'
+          id='new-password'
+          ref={newPasswordInputRef}
+        />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
