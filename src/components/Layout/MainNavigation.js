1be49404dff956bc03456cba401467ede3d5b9ca
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-22 10:08:16 +0300

    Created authCtx

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,10 +1,13 @@
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import AuthContext from '../store/auth-context';
 
 import classes from './MainNavigation.module.css';
 
 const MainNavigation = () =>
 {
+  const authCtx = useContext(AuthContext)
+
   return (
     <header className={classes.header}>
       <Link to='/'>
